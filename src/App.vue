ca3b77fc6ce5c5f7be69f39696e5fc8ccd2ec697
steeltx <realsteel.gnu@gmail.com>
2021-04-18 19:46:32 -0500

    Se agrego bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <h1>Simulador de Tweets</h1>
+  <div class="alert alert-primary" role="alert">
+    A simple primary alert—check it out!
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
