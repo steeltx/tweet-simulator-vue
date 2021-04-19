9554417d90fc12aa1b4e852b5a472c01a3c63564
steeltx <realsteel.gnu@gmail.com>
2021-04-18 20:06:51 -0500

    Hooks para formulario

---
+++ src/hooks/useFormTweet.js
@@ -0,0 +1,15 @@
+import { ref } from "vue";
+
+export default function useFormTweet() {
+    
+    const showForm = ref(false);
+
+    const openCloseForm = () => {
+        showForm.value = !showForm.value;
+    }
+
+    return {
+        showForm,
+        openCloseForm
+    }
+}
