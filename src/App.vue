36fcf598510fe3eebc04522afef4b3e9427eabef
steeltx <realsteel.gnu@gmail.com>
2021-04-21 00:20:19 -0500

    Componente para mostrar tweets

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <Menu :openCloseForm="openCloseForm" :showForm="showForm" />
   <TweetForm :showForm="showForm"/>
+  <TweetList/>
 </template>
 
 <script>
 import Menu from "./components/Menu";
 import TweetForm from "./components/TweetForm";
 import useFormTweet from "./hooks/useFormTweet";
+import TweetList from "./components/TweetList";
 
 export default {
   name: "App",
   components: {
     Menu,
     TweetForm,
+    TweetList
   },
   setup() {
     return {
